c7eafbfbd44288c0f85a359923692c2be24d87a7
paramgupta107 <param.gupta107@gmail.com>
2020-04-06 16:35:45 +0530

    switch to flask security

--- app.py
+++ app.py
@@ -1,96 +1,60 @@
-from flask import Flask, render_template, flash, redirect, url_for
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, login_required, current_user
 from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, RadioField, IntegerField, DecimalField, SelectField, PasswordField, SubmitField
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask_security import Security, UserMixin, RoleMixin, SQLAlchemyUserDatastore, login_required
+from flask_security.utils import hash_password
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dfdajfriaejfidfonoi'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Param123@localhost/png'
+app.config['SECURITY_PASSWORD_SALT'] = 'dfdgsdfsadfasg'
 db = SQLAlchemy(app)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-login = LoginManager(app)
 Bootstrap(app)
-app.config['SECRET_KEY'] = 'dsfsgregsd'
 
-class User(UserMixin, db.Model):
-    """Model for user accounts."""
+roles_users = db.Table('roles_users', db.Column('user_id', db.Integer, db.ForeignKey('user.id')), db.Column('role_id', db.Integer, db.ForeignKey('role.id')))
 
-    __tablename__ = 'flasklogin-users'
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(100), unique = True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean)
+    confirmed_at = db.Column(db.DateTime)
+    roles = db.relationship('Role', secondary =roles_users, backref = db.backref('users', lazy = 'dynamic'))
 
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(40))
+    description = db.Column(db.String(255))
 
-    id = db.Column(db.String(10),
-                     nullable=False,
-                     unique=True,
-                     primary_key=True)
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
 
-    password = db.Column(db.String(200),
-                         primary_key=False,
-                         unique=False,
-                         nullable=False)
+@app.route('/')
+def home():
+    return render_template('login.html')
 
-    def set_password(self, password):
-        """Create hashed password."""
-        self.password = generate_password_hash(password, method='sha256')
+@app.route('/abc')
+def abc():
+    #user_datastore.create_user(email = 'ab@a.com', password = hash_password('abcd'))
+    #db.session.commit()
+    return "aa"
 
-    def check_password(self, password):
-        """Check hashed password."""
-        return check_password_hash(self.password, password)
-
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
-
-
-
-
-
-
-
-@login.user_loader
-def load_user(username):
-    return User.query.get(username)
-
-
-class SignupForm(FlaskForm):
-    """User Signup Form."""
-    name = StringField('Name')
-    email = StringField('Email')
-    password = PasswordField('Password')
-    submit = SubmitField('Register')
-
-class LoginForm(FlaskForm):
-    """User Login Form."""
-    username = StringField('Email')
-    password = PasswordField('Password')
-    submit = SubmitField('Log In')
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login_page():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(id=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password',category="danger")
-            return redirect(url_for('login_page'))
-        login_user(user)
-        return redirect(url_for('index'))
-    return render_template('login.html', form=form)
-
-@app.route('/index')
+@app.route('/aa')
 @login_required
-def index():
-    return current_user.id
+def aa():
+    return 'test'
 
 if __name__ == "__main__":
+    app.run(debug=True)
+
+
+    #flask shell
     # db.create_all()
-    # u = User(id='param')
-    # u.set_password('Param123')
+    # u = User(email='dfdfd.dfdf@sd.com')
+    # 
     # db.session.add(u)
     # db.session.commit()
 
-    app.run(debug = True)
 
     #Todo 
     # flask security
@@ -99,3 +63,4 @@ if __name__ == "__main__":
     #flask uploads
     #comment is inserted
     #second comment
+
